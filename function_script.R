55189c297fbb023ccfbbe69f152c70320660f602
Sara Schreder <sis0004@mix.wvu.edu>
2019-11-04 15:07:01 -0500

    End of session

--- function_script.R
+++ function_script.R
@@ -1,6 +1,23 @@
+#load packages
+library(gapminder)
+
+#read in .csv file
+co2perCap <- read.csv(file="data/co2_emissions_tonnes_per_person_gapminder.csv")
+
+# source in script
+source("")
+
+#function to find the co2 per capita for a country in a given year
+co2_report <- function(dat, country, year){
+  cntry <- dat[dat$country==country, ]
+  yr <- dat[cntry == year, ]
+  report <- yr
+  return(report)
+}
+
 co2_report <- function(dat, country, year){
   dat <- dat[dat$country==country, ]
-  dat <- dat[dat$year == year, ]
+  dat <- dat[dat$year==year, ]
   report <- dat
   return(report)
 }
